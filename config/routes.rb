89cc448fb2dd60e67336f9866090823b7581ccf3
aya.murata <aya.murata093@gmail.com>
2018-01-14 06:45:50 +0000

    tasklist toppage2

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   get 'users/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'tasks#index'
+
+  root to: 'toppages#index'
+
   
   resources :tasks
   
