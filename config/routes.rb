3b7b4df8dcc6191834ddd3e1ee00bf5965e4ee69
aya.murata <aya.murata093@gmail.com>
2018-01-14 08:13:22 +0000

    tasklist login

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,14 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  root to: 'toppages#index'
 
-  get 'users/index'
+  get 'login', to: 'sessions#new'
 
-  get 'users/show'
+  post 'login', to: 'sessions#create'
 
-  get 'users/new'
-
-  get 'users/create'
+  delete 'logout', to:'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'toppages#index'
-
-  
   resources :tasks
   
   get 'signup', to: 'users#new'
